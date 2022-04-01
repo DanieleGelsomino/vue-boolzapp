3052f19b3f3d021cc524cc7444b3c6ab1c54b215
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-04-01 11:41:33 +0200

    add  milestone text

--- js/script.js
+++ js/script.js
@@ -7,6 +7,23 @@ console.log("Vue Js ok!");
     visualizzare nome e immagine di ogni contatto
     QUINDI solo la parte a sinistra sarà dinamica (per mostrare la lista contatti),
     mentre a destra ci sarà HTML statico, uguale a quello presentato nel layout
+    Milestone 2
+    ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+    messaggi relativi al contatto attivo all’interno del pannello della conversazione.
+    ● Click sul contatto mostra la conversazione del contatto cliccato.
+    Milestone 3
+    ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+    “enter” il testo viene aggiunto al thread sopra, come messaggio verde.
+    ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+    un “ok” come risposta, che apparirà dopo 1 secondo.
+    Milestone 4
+    ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+    contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+    “mar” rimangono solo Marco e Martina).
+    Milestone 5 - opzionale
+    ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+    permette di cancellare il messaggio selezionato.
+    ● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti. 
 */
 let contacts = [
   {
@@ -181,6 +198,8 @@ const app = new Vue({
     newMessage: "",
     // key per cercare nomi nella lista contatti
     search: "",
+
+    activeMenu: 0,
   },
   methods: {
     imgUrlContact(imgContacts) {
